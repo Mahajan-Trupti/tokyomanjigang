10228a044087523bd211081cc768a2712bc4fd76
mahajan-trupti <truptimahajan789@gmail.com>
2025-08-04 23:53:56 +0530

    fixed navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Navigation from "./components/Navigation";
+import Background from "./components/Background";
 import HomePage from "./components/HomePage";
+import AboutUs from "./components/AboutUs";
 
 const App = () => {
   return (
     <Router>
+      <Background />
+      <Navigation />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<HomePage />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </Router>
   );
